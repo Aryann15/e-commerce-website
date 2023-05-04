ad9cce0e9336479e59abebb7e98d0c6552c6923a
Aryann15 <rajlysm15@gmail.com>
2023-05-04 12:32:29 +0530

    creating cart and shop pages

---
+++ src/pages/Shop/Shop.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const Shop = () => {
+  return (
+    <div className="shop">
+        <div>
+            <h1>AVENGERS SHOP</h1>
+        </div>
+        <div className="product">
+            
+        </div>
+    </div>
+  )
+}
