a6ba4c341d2e12d710bb67303432b41060b28d6b
Aryann15 <rajlysm15@gmail.com>
2023-05-04 11:52:05 +0530

    initialised browser routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import  Navbar  from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
-      
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" />
+          <Route path="/cart" />
+        </Routes>
+      </Router>
     </div>
   );
 }
