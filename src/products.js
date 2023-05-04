5169c9758d4124dadaa474287e7d3f140ed1fa25
Aryann15 <rajlysm15@gmail.com>
2023-05-04 12:41:32 +0530

    creating an api for the porduct imfo

---
+++ src/products.js
@@ -0,0 +1,72 @@
+import product1 from "./assets/1.jpeg";
+import product2 from "./assets/2.jpeg";
+import product3 from "./assets/3.jpg";
+import product4 from "./assets/4.jpeg";
+import product5 from "./assets/5.avif";
+import product6 from "./assets/6.jpg";
+import product7 from "./assets/7.png";
+import product8 from "./assets/8.jpg";
+import product9 from "./assets/9.jpeg";
+
+export const PRODUCTS = [
+  {
+    id: 1,
+    prodcutName: "TOBEY SUIT",
+    price: 2000,
+    productImage: product1,
+  },
+  {
+    id: 2,
+    prodcutName: "ANDREW SUIT",
+    price: 1500,
+    productImage: product2,
+  },
+  {
+    id: 3,
+    prodcutName: "TOM SUIT",
+    price: 1600,
+    productImage: product3,
+  },
+  {
+    id: 4,
+    prodcutName: "IRON SPIDER SUIT",
+    price: 2200,
+    productImage: product4,
+  },
+  {
+    id: 5,
+    prodcutName: "CAP SHIELD",
+    price: 3000,
+    productImage: product5,
+  },
+  {
+    id: 6,
+    prodcutName: "MJOLNIR",
+    price: 3500,
+    productImage: product6,
+  },
+  {
+    id: 7,
+    prodcutName: "STORMBREAKER",
+    price: 3300,
+    productImage: product7,
+  },
+  {
+    id: 8,
+    prodcutName: "IRON MAN SUIT",
+    price: 10000,
+    productImage: product8,
+  },
+  {
+    id: 9,
+    prodcutName: "LOKI SCEPTRE",
+    price: 2500,
+    productImage: product9,
+  },
+  {
+    id: 1,
+    prodcutName: "TOBEY SUIT",
+    price: 2000,
+    productImage: product1,
+  },
+];
